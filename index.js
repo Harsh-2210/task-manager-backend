9b18ce79b96fd192bf44fc030b8faf909b9012c5
Harsh-2210 <harshsrivastavaofficial22@gmail.com>
2025-07-22 23:22:16 +0530

    new backend

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 const express = require('express');
-const cors = require('cors');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-// require('dotenv').config();
-import dotenv from 'dotenv';
-dotenv.config();
+const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
-const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
-
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5174', // if used during testing
-  'https://task-manager-authentication.netlify.app'
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-app.options('*', cors());
-
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
-app.get('/', (req, res) => res.send('Task Manager API is live '));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('Task Manager API Running');
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
